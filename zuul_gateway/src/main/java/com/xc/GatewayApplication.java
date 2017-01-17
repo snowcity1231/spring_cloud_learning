dfd92b879d4835407055b2b4205353c995524cab
snowcity1231 <xuechen911231@aliyun.com>
2017-01-17 16:22:58 +0800

    zuul 整合 ribbon

--- zuul_gateway/src/main/java/com/xc/GatewayApplication.java
+++ zuul_gateway/src/main/java/com/xc/GatewayApplication.java
@@ -2,12 +2,8 @@ package com.xc;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 import com.xc.filter.AccessFilter;
 
