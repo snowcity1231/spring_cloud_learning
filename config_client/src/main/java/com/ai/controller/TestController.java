d21c7515f52ce3cf9e54f6763893f70f3fe20983
snowcity1231 <xuechen911231@aliyun.com>
2017-01-18 17:22:27 +0800

    修改配置

--- config_client/src/main/java/com/ai/controller/TestController.java
+++ config_client/src/main/java/com/ai/controller/TestController.java
@@ -1,5 +1,13 @@
 package com.ai.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +30,52 @@ public class TestController {
 	@RequestMapping("/from")
 	public String from() {
 		return from;
+	}
+	
+	/**
+	 * 模拟post请求到http://localhost:7002/refresh，刷新配置值
+	 */
+	@RequestMapping(value = "post")
+	public String post() {
+		PrintWriter out = null;
+		BufferedReader in = null;
+		String result = "";
+		
+		try {
+			URL url = new URL("http://localhost:7002/refresh");
+			URLConnection conn = url.openConnection();
+			conn.setRequestProperty("accept", "*/*");
+			conn.setRequestProperty("connection", "Keep-Alive");
+			//post请求
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			
+			out = new PrintWriter(conn.getOutputStream());
+			out.println();
+			out.flush();
+			in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			while((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(out != null) {
+					out.close();
+				}
+				if(in != null) {
+					in.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		return result;
 	}
 }
