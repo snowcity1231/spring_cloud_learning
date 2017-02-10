1bd65b2341aa7a5be6752b1bfb5077b0da92aa64
snowcity1231 <xuechen911231@aliyun.com>
2017-02-10 17:11:07 +0800

    通过RabbitMQ实现多个客户端的配置实时更新

--- config_client/src/main/java/com/ai/controller/TestController.java
+++ config_client/src/main/java/com/ai/controller/TestController.java
@@ -8,7 +8,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -59,6 +61,41 @@ public class TestController {
 			httpResponse = httpClient.execute(httpPost);
 			httpEntity = httpResponse.getEntity();
 			response = EntityUtils.toString(httpEntity, "UTF-8");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return response;
+	}
+	
+	/**
+	 * 提交/bus/refresh实时更新消息总线上的属性配置
+	 * 只要一处客户端提交更新请求，所有config-client客户端的配置信息都会更改
+	 * @param destination 指定刷新范围，例如destination=democonfig:7002可以刷新具体实例，或者destination=democonfig:**利用路径匹配，刷新所有democonfig服务
+	 * @return
+	 */
+	@RequestMapping("/doBusRefresh")
+	public static String doBusRefresh(String destination) {
+		String response = "";
+		CloseableHttpClient httpClient = null;
+		CloseableHttpResponse httpResponse = null;
+		HttpEntity httpEntity = null;
+		
+		//可以通过给/bus/refresh接口配置参数，指定刷新反范围
+		String url = "http://localhost:7002/bus/refresh";
+		if(!StringUtils.isBlank(destination)){
+			url += "?destination=" + destination;
+		}
+		
+		try {
+			httpClient = HttpClients.createDefault();
+			HttpPost httpPost = new HttpPost(url);
+			httpResponse = httpClient.execute(httpPost);
+			httpEntity = httpResponse.getEntity();
+			response = EntityUtils.toString(httpEntity, "UTF-8");
+			
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
